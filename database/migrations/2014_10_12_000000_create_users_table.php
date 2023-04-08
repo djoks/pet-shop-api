abe0bf0d706f6b3fef8a4016d67e47ca1587ddd1
djoks <contact.djoks@gmail.com>
2023-04-08 17:17:20 +0000

    Feat: Create Database Migrations
    - Create users table migration.
    - Create password resets table migration.
    - Create jwt tokens table migration.
    - Create categories table migration.
    - Create products table migration.
    - Create order statuses table migration.
    - Create payments table migration.
    - Create orders table migration.
    - Create files table migration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,12 +13,19 @@ return new class () extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('uuid')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('is_admin')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar')->unique()->nullable();
+            $table->string('address');
+            $table->string('phone_number');
+            $table->boolean('is_marketing')->default(0);
             $table->timestamps();
+            $table->timestamp('last_login_at')->nullable();
         });
     }
 
