16ab3956fdff51eb5ae83245557ddc4289bece56
djoks <contact.djoks@gmail.com>
2023-04-13 21:59:45 +0000

    Feat: Implement UserService.
    - Implement function to find user by uuid.
    - Implement function to check if user is an admin.

---
+++ app/Services/UserService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Lcobucci\JWT\Token;
+use App\Models\JwtToken;
+use Lcobucci\JWT\Validation\Validator;
+use Lcobucci\JWT\Validation\Constraint\RelatedTo;
+
+class UserService
+{
+    protected \App\Services\TokenService $tokenService;
+
+    public function __construct(\App\Services\TokenService $tokenService)
+    {
+        $this->tokenService = $tokenService;
+    }
+
+    /**
+     * @return App\Models\User
+     */
+    public function getByUuid(string $uuid)
+    {
+        $user = User::where('uuid', $uuid)->first();
+        if (!$user) {
+            return (object) [
+                'code' => 404,
+                'message' => 'User not found.',
+            ];
+        }
+
+        return $user;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isAdmin(string $uuid)
+    {
+        $user = $this->getByUuid($uuid);
+        return $user->is_admin;
+    }
+}
